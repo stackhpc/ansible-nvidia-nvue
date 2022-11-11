770da473f8e867bae80b0f2fd821e2591df9108d
Krishna Vasudevan <kvasudevan@nvidia.com>
2022-11-12 01:45:20 +0530

    Added files for the demo setup

---
+++ examples/nvair-demo/setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y
+python3 -m pip install ansible
+export PATH=/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+
+mkdir .ansible/collections
+mkdir .ansible/collections/ansible_collections
+mkdir .ansible/collections/ansible_collections/nvidia
+cd .ansible/collections/ansible_collections/nvidia
+git clone https://gitlab.com/nvidia-networking/systems-engineering/nvue.git
+cd nvue
+git checkout devel
+
+ansible-playbook enable-api.yml -i ../hosts
