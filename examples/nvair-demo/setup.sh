6f1bad3bf07e9615298ebb665bf835a3aa4d060f
Krishna Vasudevan <kvasudevan@nvidia.com>
2022-11-14 10:39:37 +0000

    Update setup.sh

--- examples/nvair-demo/setup.sh
+++ examples/nvair-demo/setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get update -y
 python3 -m pip install ansible
 export PATH=/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+ansible --version
 
 mkdir ~/.ansible/collections
 mkdir ~/.ansible/collections/ansible_collections
