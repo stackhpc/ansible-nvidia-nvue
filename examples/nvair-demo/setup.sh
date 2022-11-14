1393f55a6fe893ca1b03663cff52f257eafc7600
Krishna Vasudevan <kvasudevan@nvidia.com>
2022-11-14 10:32:41 +0000

    Update setup.sh

--- examples/nvair-demo/setup.sh
+++ examples/nvair-demo/setup.sh
@@ -2,10 +2,10 @@ sudo apt-get update -y
 python3 -m pip install ansible
 export PATH=/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
 
-mkdir .ansible/collections
-mkdir .ansible/collections/ansible_collections
-mkdir .ansible/collections/ansible_collections/nvidia
-cd .ansible/collections/ansible_collections/nvidia
+mkdir ~/.ansible/collections
+mkdir ~/.ansible/collections/ansible_collections
+mkdir ~/.ansible/collections/ansible_collections/nvidia
+cd ~/.ansible/collections/ansible_collections/nvidia
 git clone https://gitlab.com/nvidia-networking/systems-engineering/nvue.git
 cd nvue
 git checkout devel
