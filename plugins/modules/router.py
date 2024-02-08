1fe779c7dbc70559838c1d4499eeb9fd40219cef
Alexander Dibbo <alexander.dibbo@stfc.ac.uk>
2024-02-08 09:16:19 +0000

    Add functionality for graceful restart
    This is needed for the warm and fast reboot modes

--- plugins/modules/router.py
+++ plugins/modules/router.py
@@ -59,6 +59,22 @@ options:
                         description: ASN for all VRFs, if a single AS is in use. If "none", then ASN must be set for every VRF. This is the default.
                         required: false
                         type: int
+                    graceful_restart:
+                        description: BGP Graceful restart global configuration.
+                        type: dict
+                        required: false
+                        suboptions:
+                            mode:
+                                description: Role of router during graceful restart.
+                                             helper-only, router is in helper role.
+                                             full, router is in both helper and restarter role.
+                                             off, GR is disabled for the router
+                                type: str
+                                required: false
+                                choices:
+                                    - 'full'
+                                    - 'off'
+                                    - 'helper-only'
                     router_id:
                         description: BGP router-id for all VRFs, if a common one is used. If "none", then router-id must be set for every VRF.
                                      This is the default.
@@ -194,6 +210,9 @@ def main():
         bgp=dict(type='dict', required=False, options=dict(
             enable=dict(type='str', required=False, default='off', choices=['on', 'off']),
             autonomous_system=dict(type='int', required=False),
+            graceful_restart=dict(type='dict', required=False, options=dict(
+                mode=dict(type='str', required=False, choices=['full', 'off', 'helper-only'])
+            )),
             router_id=dict(type='str', required=False)
         )),
         ospf=dict(type='dict', required=False, options=dict(
